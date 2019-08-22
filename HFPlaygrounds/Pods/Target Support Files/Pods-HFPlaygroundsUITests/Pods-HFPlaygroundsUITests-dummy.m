5e8c9516d47b80d41f27b8e394640ccaf7a11655
Heference <>
2019-08-22 09:19:15 +0900

    # Third Party Framework용 Playground 분기처리

--- HFPlaygrounds/Pods/Target Support Files/Pods-HFPlaygroundsUITests/Pods-HFPlaygroundsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HFPlaygroundsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_HFPlaygroundsUITests
-@end
---
+++ HFProjectPlayground/HFProjectPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HFProjectPlayground
+//
+//  Created by Heference on 22/08/2019.
+//  Copyright © 2019 sfp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
